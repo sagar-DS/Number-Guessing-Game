180bb59b0060d59eb9347d0ac5d2514afd3ad1e0
sagar-DS <sagarrjsv@gmail.com>
2025-08-15 15:50:51 +0530

    Update project1.py

--- project1.py
+++ project1.py
@@ -3,14 +3,17 @@ import random
 target = random.randint(1,100)
 
 while True:
-    rannum = int(input("Guess the number..."))
+    try:
+        rannum = int(input("Guess the number..."))
 
-    if (rannum == target ):
-        print("Success, You have guessed the number!!")    
-        break
-    elif(rannum > target):
-        print("you have guessed a larger number.Guess a smaller number.")
-    else:
-        print("you have guessed a smaller number.Guess a bigger number.")
+        if (rannum == target ):
+            print("Success, You have guessed the number!!")
+            break
+        elif(rannum > target):
+            print("you have guessed a larger number.Guess a smaller number.")
+        else:
+            print("you have guessed a smaller number.Guess a bigger number.")
 
+    except ValueError:
+        print("That's not a number! Please try again.")
         
